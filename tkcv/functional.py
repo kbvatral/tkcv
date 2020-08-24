8f92f67759fb3dee967128838ee87f0bb5caf3c4
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2020-08-23 22:11:25 -0500

    Add media file open dialog

--- tkcv/functional.py
+++ tkcv/functional.py
@@ -1,5 +1,8 @@
+from .utils import try_open_img, get_image_filetypes, get_video_filetypes
 from .window import Window
-from tkinter import Tk
+import tkinter.filedialog
+import tkinter as tk
+import cv2
 
 def imshow(window, image, time=0):
     if isinstance(window, str):
@@ -26,3 +29,23 @@ def destroyAllWindows():
     for _, window in Window._active_windows.items():
         window.destroy()
     Window._active_windows.clear()
+
+def open_image(title="Open Image", include_video=False):
+    image_formats = get_image_filetypes(to_string=True)
+    video_formats = get_video_filetypes(to_string=True)
+
+    formats = [("Image Files", image_formats)]
+    if include_video:
+        formats = [("Media Files", image_formats+" "+video_formats)] + formats
+        formats.append(("Video Files", video_formats))
+    formats.append(("All Files", "*.*"))
+
+    path = tk.filedialog.askopenfilename(title=title, filetypes=formats)
+    return try_open_img(path, try_video=include_video)
+
+def open_video(title="Open Video"):
+    video_formats = get_video_filetypes(to_string=True)
+    formats = [("Video Files", video_formats), ("All Files", "*.*")]
+    
+    path = tk.filedialog.askopenfilename(title=title, filetypes=formats)
+    return cv2.VideoCapture(path)
--- tkcv/utils.py
+++ tkcv/utils.py
@@ -1,6 +1,28 @@
 import cv2
 from PIL import Image, ImageTk
 
+image_filetypes = [".jpg", ".jpeg", "jpe", ".jp2", ".png", ".webp", ".pbm", ".pgm", ".ppm",
+                   ".pxm", ".pnm", ".pfm", ".sr", ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic", ".bmp", ".dib"]
+video_filetypes = [".avi", ".mp4", ".avchd", ".mkv", ".mov", ".gt", ".flv"]
+
+def get_image_filetypes(to_string=False):
+    if to_string:
+        image_formats = ""
+        for fmt in image_filetypes:
+            image_formats += fmt + " "
+        return image_formats[:-1]
+    else:
+        return image_filetypes
+
+def get_video_filetypes(to_string=False):
+    if to_string:
+        video_formats = ""
+        for fmt in video_filetypes:
+            video_formats += fmt + " "
+        return video_formats[:-1]
+    else:
+        return video_filetypes
+
 def cv2Tk(image, swapRB=True):
     if len(image.shape) == 3 and swapRB:
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -8,3 +30,28 @@ def cv2Tk(image, swapRB=True):
         img = image
     img_pil = ImageTk.PhotoImage(Image.fromarray(img))
     return img_pil
+
+
+def try_open_img(path, try_video=True):
+    img = None
+    try:
+        candidate = cv2.imread(path)
+        if candidate is not None and candidate.size != 0:
+            img = candidate
+    except:
+        pass
+    # If not image, try opening as video
+    if img is None and try_video:
+        vs = None
+        try:
+            vs = cv2.VideoCapture(path)
+            _, candidate = vs.read()
+            if candidate is not None and candidate.size != 0:
+                img = candidate
+        except:
+            pass
+        try:
+            vs.close()
+        except:
+            pass
+    return img
