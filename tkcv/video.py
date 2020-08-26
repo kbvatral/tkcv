672a714fdac62fcd3af11d00e7d871f17ceb22fa
Caleb Vatral <caleb.m.vatral@vanderbilt.edu>
2020-08-26 15:35:15 -0500

    Add simple video player class

---
+++ tkcv/video.py
@@ -0,0 +1,80 @@
+import cv2
+import tkinter.ttk
+import tkinter as tk
+from .window import Window
+
+def count_frames(video):
+    release = False
+    if not isinstance(video, cv2.VideoCapture):
+        video = cv2.VideoCapture(video)
+        release = True
+    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    try:
+        total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    except:
+        total = 0
+        while True:
+            (grabbed, frame) = video.read()
+            if not grabbed:
+                break
+            total += 1
+
+    if release:
+        video.release()
+    else:
+        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    return total
+
+
+class VideoPlayer(tk.Frame):
+    def __init__(self, stream, *args, override_fps=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vs = stream
+        if override_fps is None:
+            self.fps = max(1, self.vs.get(cv2.CAP_PROP_FPS))
+        else:
+            self.fps = override_fps
+        self.frame_time = int(1000 // self.fps)
+        self.frame_total = count_frames(self.vs)
+        self.frame_counter = 0
+
+        self.main_window = Window("")
+        self.main_window.pack()
+        self.progress_bar = tk.ttk.Progressbar(mode='determinate', length=int(0.9*self.vs.get(cv2.CAP_PROP_FRAME_WIDTH)))
+        self.progress_bar.pack(pady=10)
+        self.ctrl_bar = tk.Frame()
+        self.ctrl_bar.pack(pady=10)
+        
+        self.btn_next = tk.Button(text="<<", master=self.ctrl_bar, command=lambda: self._seekFrame(self.frame_counter-1))
+        self.btn_next.grid(row=1, column=1, padx=5)
+        self.btn_play = tk.Button(text="Play/Pause", master=self.ctrl_bar, command=self._playPauseCallback)
+        self.btn_play.grid(row=1, column=2, padx=5)
+        self.btn_next = tk.Button(text=">>", master=self.ctrl_bar, command=self._nextFrame)
+        self.btn_next.grid(row=1, column=3, padx=5)
+
+        self.playing = False
+        self._nextFrame()
+
+    def get_frame(self):
+        ret, frame = self.vs.read()
+        return frame
+
+    def _playPauseCallback(self):
+        self.playing = not self.playing
+        if self.playing:
+            self._nextFrame()
+
+    def _nextFrame(self):
+        self.frame_counter += 1
+        if self.frame_counter <= self.frame_total:
+            self.progress_bar['value'] = 100*self.frame_counter/self.frame_total
+            frame = self.get_frame()
+            self.main_window.set_img(frame)
+        if self.playing:
+            self.after(self.frame_time, self._nextFrame)
+
+    def _seekFrame(self, frame_num):
+        self.frame_counter = frame_num-1
+        self.vs.set(cv2.CAP_PROP_POS_FRAMES, self.frame_counter)
+        self._nextFrame()
--- tkcv/window.py
+++ tkcv/window.py
@@ -1,17 +1,18 @@
-from tkinter import Frame, Label
+import tkinter as tk
 from .utils import cv2Tk
 
-class Window(Frame):
+class Window(tk.Frame):
     _active_windows = {}
     def __init__(self, name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name
-        self.master.title(name)
-        self.impanel = Label()
+        Window._active_windows[name] = self
+        self.impanel = tk.Label()
         self.impanel.pack()
 
-        Window._active_windows[name] = self
-        self.master.protocol("WM_DELETE_WINDOW", self.close)
+        if isinstance(self.master, tk.Tk):
+            self.master.title(name)
+            self.master.protocol("WM_DELETE_WINDOW", self.close)
 
     def set_img(self, image):
         img = cv2Tk(image)
@@ -20,9 +21,11 @@ class Window(Frame):
 
     def close(self):
         self.master.quit()
-        self.master.withdraw()
+        if isinstance(self.master, tk.Tk):
+            self.master.withdraw()
 
     def mainloop(self, *args, **kwargs):
-        self.master.deiconify()
+        if isinstance(self.master, tk.Tk):
+            self.master.deiconify()
         super().mainloop(*args, **kwargs)
     
